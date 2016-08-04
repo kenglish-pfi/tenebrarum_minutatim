6898ff690061847b05da4df7071f7bf61bdc6ecb
kenglish <kevin.english@soteradefense.com>
2016-08-04 18:08:17 -0500

    Can now work as a stdin to stdout filter with proper Unicode setup of stdin and stdout

--- polished/norm_unicode/norm_unicode.py
+++ polished/norm_unicode/norm_unicode.py
@@ -3302,8 +3302,15 @@ def normalizeText(unicodeText):
 if __name__ == "__main__":
 
     import sys
+    import codecs
     if len(sys.argv) > 1:
         print u" ".join(map(normalizeText, sys.argv[1:]))
+    else:
+        sys.stdout=codecs.getwriter('UTF-8')(sys.stdout)
+        sys.stdin=codecs.getreader('UTF-8')(sys.stdin)
+        for line in sys.stdin:
+            line.rstrip()
+            print(normalizeText(line))
        
 # fi
     
