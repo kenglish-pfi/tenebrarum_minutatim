31044c2dce4adf7825067444f7f190e1765a57a7
kenglish <kevin.english@soteradefense.com>
2016-08-13 13:56:31 -0500

    Rewrite using using circular/hexagon decomposition method.

--- unpolished/SphericalGeoHash/spherical_geohash.py
+++ unpolished/SphericalGeoHash/spherical_geohash.py
@@ -10,59 +10,79 @@ basi = [
     A([ A([0,0,0]), A([(e*e),(e*e),(-e)]),  A([(e*e*e-e*e),(e*e*e+e*e),(e*e)]), A([(e*e*e+e*e),(e*e*e-e*e),(e*e)]), 
                    -A([(e*e),(e*e),(-e)]),  -A([(e*e*e-e*e),(e*e*e+e*e),(e*e)]), -A([(e*e*e+e*e),(e*e*e-e*e),(e*e)]) ])
 ]
-factor = 1.0/math.sqrt(2.0)
-skip = A([0,0,0])
-        
-def soln_value(soln):
-    accum = [0,0,0]
-    for v in soln:
-        accum[0] = accum[0] + v[0]
-        accum[1] = accum[1] + v[1]
-        accum[2] = accum[2] + v[2]
-    denom = math.sqrt( (accum[0]*accum[0]) + (accum[1]*accum[1]) + (accum[2]*accum[2]) )
-    accum[0] = accum[0] / denom
-    accum[1] = accum[1] / denom
-    accum[2] = accum[2] / denom
-    return A(accum)
 
-def distance(objective, soln):
-    Z = soln_value(soln)
-    pre_dist = objective - Z
-    dist = math.sqrt((pre_dist[0]*pre_dist[0]) + (pre_dist[1]*pre_dist[1]) + (pre_dist[2]*pre_dist[2]))
-    print str(dist) + " := " +repr(Z) + " <=> " + repr(objective)
-    return dist
+def skew_symetric_cross(v):
+    return numpy.matrix( [ [0, -v[2], v[1]] , [v[2], 0, -v[0]] , [-v[1], v[0], 0] ])
 
+# http://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
+def rot_x_matrix(current_vector):
+    V = numpy.cross(current_vector, basi[0][1])
+    s = numpy.linalg.norm(V)
+    if s < 0.00000000000000000001:
+        return numpy.identity(3)
+    c = numpy.inner(current_vector, basi[0][1])
+    Vx = skew_symetric_cross(V)
+    R = Vx + numpy.identity(3) + (numpy.dot(Vx, Vx) * ((1 - c) / (s * s)))
+    return R
     
-def nibble2vect(orientation, index, letter):
-    basis_idx = ord(letter) - ord('0')
-    if (index & 1) == orientation :
-        vect = basi[1][basis_idx]
+# we need 6 evenly spaced points on a circle perpendicular to the current vector
+# At the first iteration we can imagine a cube inside the sphere constructed such
+# that just the points of the corners touch the sphere.  The cube's transecting diagonals
+# are then the diameter of the sphere = 2.
+# The coordinates of the points for one basis are the points of the other basis.
+# Each vector in one base will have four vectors in the other base that are closest
+# to it.  
+    
+def generate_center(current_vector, level, letter):   
+    idx = ord(letter) - ord('0')
+    angles = [0.0, math.pi/3.0, 2.0*math.pi/3.0, math.pi, 4.0*math.pi/3.0, 5.0*math.pi/3.0]
+    
+    if idx == 0:
+        return current_vector
+    theta = angles[idx - 1]
+    
+    R = rot_x_matrix(current_vector)
+    W = numpy.array([1.0, math.cos(theta)/e, math.sin(theta)/e]) / math.sqrt(3) 
+    X = (W * R).A[0]  # 3x3 Matrix * 1x3 Array ==> 1x3 Matrix ... grab the one and only 1-D vector from the 1-D matrix
+    print repr(X)
+    if level > 1:
+        for i in range(level-1):
+            X = current_vector + X
+            X = X / numpy.linalg.norm(X)  # normalize, not divide by 2.  Divide by 2 gives the middle point on the chord and not on the sphere.
+    print repr(X)
+    return X
+
+
+def distance(Vect, Wect):
+    V = numpy.cross(Vect, Wect)
+    s = numpy.linalg.norm(V)
+    return numpy.arcsin(s)
+    
+    
+def nibble2vect(level, letter, current_vector=numpy.array([0.0,0.0,0.0])):
+    idx = ord(letter) - ord('0')
+    if level == 0:
+        vect = basi[0][idx]
     else:
-        vect = basi[0][basis_idx]
-        
-    if index > 0:
-        pwr = index/2  # assuming integer truncation
-        vect = vect * (0.5 * math.pow(factor, pwr))
-    
+        vect = generate_center(current_vector, level, letter)
     return vect
     
-def refine(objective, epsilon, orientation, soln=[], current_error=2.0, hash=""):
-    N = len(soln)
-    if N > 29 :
+def refine(objective, epsilon, current_vector=numpy.array([0.0,0.0,0.0]), current_error=2.0, hash=""):
+    level = len(hash)
+    if level > 29 :
         print "Failed to converge:"
-        print "   refine(" + ", ".join([ repr(objective), str(epsilon), str(orientation), repr(soln), str(current_error), hash]) + ")"
+        print "   refine(" + ", ".join([ repr(objective), str(epsilon), repr(current_vector), str(current_error), hash]) + ")"
         return hash + "7"
 
     letters = ['0', '1', '2', '3', '4', '5', '6']
     min_hash = hash
     min_dist = current_error * 1.0000001  # need to epsilon this up so that the constraint "dist < min_dist" can find next steps pairs that end in [0,0,0]
     
-    if N == 0:
+    if level == 0:
         min_U = A([0,0,0])
         for u in letters[1:]:  # Very important to skip zero-vector the first time
-            U = nibble2vect(orientation, 0, u)
-            attempt = soln + [U]
-            dist = distance(objective, attempt)
+            U = nibble2vect(0, u)
+            dist = distance(objective, U)
             if dist < min_dist:
                 min_dist = dist
                 min_hash = hash + u
@@ -70,53 +90,30 @@ def refine(objective, epsilon, orientation, soln=[], current_error=2.0, hash="")
                 
         current_error = min_dist
         hash = min_hash
-        return refine(objective, epsilon, orientation, soln + [min_U], current_error, hash)
+        return refine(objective, epsilon, min_U, current_error, hash)
         
     else:
-        # We look ahead 2 steps for minimal error and keep the first 1 on each iteration
-#        min_U = A([0,0,0])
-#        for u in letters:
-#            U = nibble2vect(orientation, N, u)
-#            for v in letters:
-#                V = nibble2vect(orientation, N+1, v)
-#                attempt = soln + [U, V]
-#                dist = distance(objective, attempt)
-#                if dist < min_dist:
-#                    min_dist = dist
-#                    min_hash = hash + u + v
-#                    min_U = U
-#                    
-        min_U = A([0,0,0])
-        min_V = A([0,0,0])
+        min_U = current_vector
         for u in letters:
-            U = nibble2vect(orientation, N, u)
-            for v in letters:
-                V = nibble2vect(orientation, N+1, v)
-                for w in letters:
-                    W = nibble2vect(orientation, N+2, w)
-                    for x in letters:
-                        X = nibble2vect(orientation, N+3, x)
-                        attempt = soln + [U, V, W, X]
-                        dist = distance(objective, attempt)
-                        if dist < min_dist:
-                            min_dist = dist
-                            min_hash = hash + u + v + w + x
-                            min_U = U
-                            min_V = V
-                            print min_hash + " : " + str(min_dist) + " " + repr(attempt)
+            U = nibble2vect(level, u, current_vector)
+            dist = distance(objective, U)
+            if dist < min_dist:
+                min_dist = dist
+                min_hash = hash + u
+                min_U = U
                                     
         # print "dist=" + str(min_dist) + " for " + repr(soln + [min_vect]) 
         if min_dist < epsilon:
             return min_hash
         
         current_error = min_dist
-        hash = min_hash[0:-2]
-        return refine(objective, epsilon, orientation, soln + [min_U, min_V], current_error, hash)
+        hash = min_hash
+        return refine(objective, epsilon, min_U, current_error, hash)
     
 
-def ecef_geohash(x, y, z, epsilon):
+def geohash(x, y, z, epsilon):
     objective = A([x,y,z])
-    return [ refine(objective, epsilon, 0), refine(objective, epsilon, 1) ]
+    return refine(objective, epsilon)
 
 # Austin :      ['412504100002014005[0]', '52504100002014005[000]']
 # print ecef_geohash(-0.1165684, -0.8572452, 0.501540, 0.001)
@@ -150,5 +147,5 @@ def randomTest():
             print h0[1]
             print h1[1]
             
-print ecef_geohash(0.15373484,  0.99290733,  0.01481033, 0.001)
+print geohash(0.15373484,  0.99290733,  0.01481033, 0.001)
 
