a2e33c05b951365a769efdcbe29a5db29d97a6f2
kenglish <kevin.english@soteradefense.com>
2016-08-18 10:58:40 -0500

    Added verification queries after script run

--- unpolished/NewmanHacks/addInternalAttachmentIds.sh
+++ unpolished/NewmanHacks/addInternalAttachmentIds.sh
@@ -6,5 +6,23 @@
 # at this time, so this should basically run but spew a ton of warnings
 # to stderr
 export TZ=UTC
+export INDEX=shiavo
 
-python addInternalAttachmentIds.py localhost shiavo baz_att_crossref.tab baz_file_key.tab
+python addInternalAttachmentIds.py localhost $INDEX baz_att_crossref.tab baz_file_key.tab
+
+# Quick verification:
+curl -XPOST "http://localhost:9200/"$INDEX"/emails/_search?pretty" -d '{ 
+    "size": 0, 
+    "aggs" : { 
+        "distinct_id__count" : { "cardinality" : { "field" : "id" } },
+        "distinct_alt_ref_id__count" : { "cardinality" : { "field" : "alt_ref_id" } }
+    }
+}'
+curl -XPOST "http://localhost:9200/"$INDEX"/attachments/_search?pretty" -d '{ 
+    "size": 0, 
+    "aggs" : { 
+        "distinct_id__count" : { "cardinality" : { "field" : "_id" } },
+        "distinct_alt_ref_id__count" : { "cardinality" : { "field" : "alt_ref_id" } }
+    }
+}'
+
