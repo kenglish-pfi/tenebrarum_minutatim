2fa10d484b9046c3f6fe38dd1356922e32eaf5fe
kenglish-pfi <kenglish.pfi@gmail.com>
2017-01-26 17:37:44 -0600

    Created single docker container, updated tests

--- unpolished/EnhancedAttribution/dockerhub/sotera/step2_test_nlpcaffe.sh
+++ unpolished/EnhancedAttribution/dockerhub/sotera/step2_test_nlpcaffe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./data/language_model/get_lm.sh'
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; python ./examples/language_model/create_lm.py --make_data'
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./examples/language_model/train_lm.sh'
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./data/language_model/get_lm.sh'
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; python ./examples/language_model/create_lm.py --make_data'
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./examples/language_model/train_lm.sh'
 
 
 
