4497c563f0b7f00571bbeb2af606ecf116b54950
kenglish-pfi <kenglish.pfi@gmail.com>
2017-01-26 18:44:43 -0600

    Fixed typo od double-"d"

--- unpolished/EnhancedAttribution/dockerhub/sotera/step2_test_nlpcaffe.sh
+++ unpolished/EnhancedAttribution/dockerhub/sotera/step2_test_nlpcaffe.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./data/language_model/get_lm.sh'
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; python ./examples/language_model/create_lm.py --make_data'
-docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/ddev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./examples/language_model/train_lm.sh'
-
-
-
-
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./data/language_model/get_lm.sh'
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; python ./examples/language_model/create_lm.py --make_data'
+docker run -it --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 sotera/ubuntu-nvidia367.57-cuda7.5-cudnn4-nlpcaffe:1  /bin/bash -c 'cd /home/caffe-user/nlpcaffe ; ./examples/language_model/train_lm.sh'
